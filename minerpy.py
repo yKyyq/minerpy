399536f0755af14fad13d00db4e4e0885f1f84e9
yKyyq <jyotiradityaxsinghchauhan@gmail.com>
2025-01-29 16:04:53 +0530

    Add Functionality
    Check For Certifications

--- minerpy.py
+++ minerpy.py
@@ -59,3 +59,14 @@ class Site:
                 return soup.get_text()
             except Exception as e:
                 print(f"Failed To Retrieve Text: {e}")
+
+    def certCheck(self):
+        try:
+            response = requests.get(self.website, timeout=10)
+            if response.status_code == 200 and "https" in response.url:
+                return 'This Site Is Certified'
+            else:
+                return 'This Site Is Not Certified'
+        except requests.exceptions.RequestException as e:
+            print(f"SSL Check Failed: {e}")
+            return False
